d0fc2f851981b20beb5021f250059923422d1b44
R_y_a_n <ryan.baptiste@hotmail.com>
2018-04-04 17:00:26 +0200

    matrixes tic tac toes a.i.  and  and heritage

---
+++ src/hello/Player.java
@@ -0,0 +1,28 @@
+package hello;
+
+public class Player {
+	
+	private String   name;
+	private char     symbol;
+	private int      score;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public char getSymbol() {
+		return symbol;
+	}
+	public void setSymbol(char symbol) {
+		this.symbol = symbol;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+}
